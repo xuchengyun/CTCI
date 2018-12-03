c5893580ecd84f68da1552453f2e49e6c201ec4c
xuchengyun <454166448@qq.com>
2018-12-02 16:37:57 -0800

    finish chapter1

--- src/chapter1/StringRotation.java
+++ src/chapter1/StringRotation.java
@@ -5,7 +5,7 @@ public class StringRotation {
     static boolean isRoration(String s1, String s2) {
         if (s1.length() != s2.length()) return false;
         String s = s2 + s2;
-        if (isSubString(s1, s2)) {
+        if (isSubString(s, s2)) {
             return true;
         }
         return false;
